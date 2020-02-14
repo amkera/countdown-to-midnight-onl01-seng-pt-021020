e7119209d6f22e59984aba558e70c28e3821356b
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-14 04:25:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,22 +1,21 @@
-#write your code here
-
 def countdown(number)
-  while number > 0 
+  while number > 0
+   sleep 1
     puts "#{number} SECOND(S)!"
-    number -= 1 
+    number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
 countdown(12)
 
-def countdown_with_sleep(integer)
-  while integer > 0 
-    sleep 1 
-      puts "#{integer} SECOND(S)!"
-      number -= 1 
-    end
-  puts "HAPPY NEW YEAR!"
+def countdown_with_sleep(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep(12)
+countdown(12)    
